e8c6555c7985a4c8d8e9b457c25b76fc8911ebab
sz-hankus <dephan@protonmail.com>
2023-06-02 07:41:52 +0200

    Added password protection to solver

---
+++ QuizSolver/QuizSolver/Views/PasswordInput.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace QuizSolver
+{
+    /// <summary>
+    /// Interaction logic for PasswordInput.xaml
+    /// </summary>
+    public partial class PasswordInput : Window
+    {
+        private PasswordInputVM vm;
+        public PasswordInput(PasswordInputVM vm)
+        {
+            this.vm = vm;
+            this.DataContext = this.vm;
+            InitializeComponent();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            ((PasswordInputVM)this.DataContext).Password = ((PasswordBox)sender).Password;
+        }
+
+        private void ConfirmButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            this.Close();
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+            this.Close();
+        }
+    }
+}
